17b4a681ed3ba57cf942150ed9763241198cc701
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-07-25 13:17:38 +0300

    program is work now, but still develop

---
+++ Timer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest4Eyes
+{
+    public class Timer
+    {
+        public int HowOffenRest { get; set; }
+        public int HowLongRest { get; set; }
+    }
+}
